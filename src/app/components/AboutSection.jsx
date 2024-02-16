4e7a2c48fc7176f415e74ca54b5d4c681d69f65b
Newton <newton@Newtons-Mac.local>
2024-02-15 20:58:01 -0800

    moved images around

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -57,7 +57,7 @@ const AboutSection = () => {
     <section className='text-white'>
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         <Image
-          src='/images/about-image-vijay.jpg'
+          src='/about-image-vijay.jpg'
           width={500}
           height={500}
           alt='about-img'
